912a2971e17141943e8f4b22093926e093e3f3bf
waxsd100 <wakokara@wax100.io>
2022-07-06 00:01:00 +0900

    giftRedemption と giftPurchasese 用呼び出し元メソッド作成

---
+++ command/gift_purchase.py
@@ -0,0 +1,13 @@
+from model.youtube_chat_moddel import YoutubeChatModel
+
+
+class GiftRedemption(YoutubeChatModel):
+    """
+    メンバーシップギフト受信(誰かが受け取った)時の処理
+    """
+
+    def __init__(self, rcon):
+        super().__init__(rcon)
+        self.__rcon = rcon
+
+    pass
